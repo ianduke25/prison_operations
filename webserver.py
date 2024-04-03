74ad32f12825473b69d43bbdb691d17c8b4d5a11
Lance Santerre <lksanterre@dons.usfca.edu>
2024-04-03 11:35:19 -0700

    app

---
+++ webserver.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import re
+import os
+st.title('US Prison Population and Visitation')
+st.write('Nick Miller | Ian Duke | Tianyunxi (Emily) Yin | Caleb Hamblen | Lance Santerre')
+file_path = '/Users/lancesanterre/prison_proj/clean_data/total_df.csv'
+
+with open(file_path, "rb") as file:
+    btn = st.download_button(
+            label="Download Full DataSet",
+            data=file,
+            file_name="total_df.csv",
+            mime="text/csv"
+        )
+file_path_small = '/Users/lancesanterre/prison_proj/clean_data/'
+user_input = st.text_input("Enter Name of Facility").upper()
+sanitized_view_name = re.sub(r'\W+', '_', user_input)
+complete_file_path = os.path.join(file_path_small, sanitized_view_name + "_df.csv")
+if os.path.exists(complete_file_path):
+    try:
+        with open(complete_file_path, "rb") as file:
+            btn = st.download_button(
+                label=f"Download {user_input} DataSet",
+                data=file,
+                file_name=f"{sanitized_view_name}.csv",
+                mime="text/csv"
+            )
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+else:
+    if user_input:  # If user_input is not empty
+        st.warning("The specified facility name does not exist. Please try again.")
