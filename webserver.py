ce0f5bd6097b943e1b3ab3171d75ca5e115d21e0
lksanterre <102986656+lksanterre@users.noreply.github.com>
2024-04-11 11:01:59 -0700

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,17 +1,17 @@
-import os
-import re
-import requests
-import pandas as pd
-import plotly.graph_objs as go
 import streamlit as st
+import re
+import os
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+
 
 st.title('US Prison Population and Visitation')
 st.write('Nick Miller | Ian Duke | Tianyunxi (Emily) Yin | Caleb Hamblen | Lance Santerre')
 file_path = 'https://raw.githubusercontent.com/lksanterre/prison/main/clean_data/total_df.csv'
-
-
+# Simulate a simple user database
 users = {
-    "lance": "lance",
+    "usfca": "dons",
     "user2": "password2",
 }
 
@@ -40,57 +40,37 @@ if st.session_state['authenticated']:
     st.title("Secure Data Page")
     st.write("Welcome! You can now download the data.")
     
-    response = requests.get(file_path)
-    if response.ok:
-        csv_content = response.content
-        btn = st.download_button(
-            label="Download Full DataSet",
-            data=csv_content,
-            file_name="total_df.csv",
-            mime="text/csv"
-        )
-    else:
-        st.error("Failed to download the dataset.")
 
+    with open(file_path, "rb") as file:
+        btn = st.download_button(
+                label="Download Full DataSet",
+                data=file,
+                file_name="total_df.csv",
+                mime="text/csv"
+            )
     
 
-    file_path_small = 'https://raw.githubusercontent.com/lksanterre/prison/main/clean_data/'
+    file_path_small = 'https://raw.githubusercontent.com/lksanterre/prison/main/clean_data//'
     user_input = st.text_input("Enter Name of Facility").upper()
     sanitized_view_name = re.sub(r'\W+', '_', user_input)
-    complete_file_path = f"{file_path_small}{sanitized_view_name}_df.csv"
-
-    
-    # Try to fetch the file to see if it exists
-    response = requests.get(complete_file_path)
-    if response.ok:
-        # If the request was successful, enable downloading
-        csv_content = response.content
-        btn = st.download_button(
-            label=f"Download {user_input} DataSet",
-            data=csv_content,
-            file_name=f"{sanitized_view_name}.csv",
-            mime="text/csv"
-        )
+    complete_file_path = os.path.join(file_path_small, sanitized_view_name + "_df.csv")
+    if os.path.exists(complete_file_path):
+        try:
+            with open(complete_file_path, "rb") as file:
+                btn = st.download_button(
+                    label=f"Download {user_input} DataSet",
+                    data=file,
+                    file_name=f"{sanitized_view_name}.csv",
+                    mime="text/csv"
+                )
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
     else:
-        # If the request failed, show an appropriate message
-        if user_input:  # Only show the warning if the user has actually input something
-            st.warning("The specified facility name does not exist or there was an error fetching the dataset. Please try again.")
-    
+        if user_input:  # If user_input is not empty
+            st.warning("The specified facility name does not exist. Please try again.")
 
 # Visualization accessible to all users
-options = ['FMC_FORT_WORTH','FCI_BECKLEY','USP_YAZOO_CITY','FCI_BIG_SPRING','FCI_HAZELTON','FMC_DEVENS','FCI_SANDSTONE','FCI_PEKIN','FCI_YAZOO_CITY_LOW',
- 'FCI_YAZOO_CITY_MEDIUM','USP_COLEMAN_I','FCI_SAFFORD','FCI_MCKEAN','MCC_NEW_YORK','FCI_TERRE_HAUTE','FCI_THREE_RIVERS','USP_BEAUMONT','FCI_OAKDALE_II',
- 'USP_BIG_SANDY','FCI_BUTNER_LOW','FCI_DUBLIN','FCI_OXFORD','FCI_BEAUMONT_LOW','USP_ATWATER','MCC_SAN_DIEGO','FCI_VICTORVILLE_MEDIUM_II','FCI_TALLAHASSEE',
- 'FCI_ALLENWOOD_LOW','FCI_DANBURY','FCI_LOMPOC','FDC_SEATAC','USP_LEAVENWORTH','MDC_GUAYNABO','FCI_GILMER','USP_HAZELTON','FCI_MANCHESTER','FCI_OTISVILLE',
- 'FCI_PETERSBURG_LOW','FPC_DULUTH','FCI_PETERSBURG_MEDIUM','FCI_MORGANTOWN','FPC_PENSACOLA','FCI_SEAGOVILLE','FCI_ALICEVILLE','FCI_PHOENIX','FCI_ENGLEWOOD',
-'FCI_FORREST_CITY_MEDIUM','FCI_MENDOTA','FCI_BUTNER_MEDIUM_II','FCI_BUTNER_MEDIUM_I','FCI_BEAUMONT_MEDIUM','MCC_CHICAGO','FCI_COLEMAN_LOW','FCI_LA_TUNA',
- 'USP_COLEMAN_II','FCI_TALLADEGA','FCI_SCHUYLKILL','USP_LOMPOC','FCI_THOMSON','FCI_BERLIN','FCI_POLLOCK','FCI_WASECA','USP_LEE','FMC_ROCHESTER','FCI_BASTROP',
- 'USP_MARION','FPC_MONTGOMERY','MDC_LOS_ANGELES','FCI_OAKDALE_I','FCI_ASHLAND','FCI_FORREST_CITY_LOW','MCFP_SPRINGFIELD','FCI_ALLENWOOD_MEDIUM','USP_TERRE_HAUTE',
- 'FCI_JESUP','FCI_MARIANNA','FCI_ESTILL','USP_MCCREARY','FCI_BENNETTSVILLE','FCI_MCDOWELL','FCI_VICTORVILLE_MEDIUM_I','FCI_FLORENCE','FCI_ELKTON','FCI_FORT_DIX',
- 'FCI_TERMINAL_ISLAND','FCI_WILLIAMSBURG','FCI_EDGEFIELD','FCI_TUCSON','FTC_OKLAHOMA_CITY','FMC_LEXINGTON','FCI_MIAMI','USP_LEWISBURG','FCI_EL_RENO','FMC_BUTNER',
-'USP_VICTORVILLE','FCI_SHERIDAN','FCI_FAIRTON','FCI_GREENVILLE','FPC_YANKTON','FCI_MEMPHIS','FCI_HERLONG','FPC_ALDERSON','FDC_HOUSTON','FMC_CARSWELL','FCI_CUMBERLAND',
-'USP_FLORENCE_HIGH','FDC_PHILADELPHIA','USP_TUCSON','FCI_MILAN','FCI_TEXARKANA','FCI_COLEMAN_MEDIUM','USP_ALLENWOOD','MDC_BROOKLYN','USP_FLORENCE_ADMAX','FPC_BRYAN',
- 'USP_POLLOCK','USP_CANAAN','USP_THOMSON','FDC_HONOLULU','USP_ATLANTA','FDC_MIAMI','FCI_RAY_BROOK','FCI_LORETTO']
+options = [f.replace('_df.csv', '') for f in os.listdir('https://raw.githubusercontent.com/lksanterre/prison/main/clean_data/') if f.endswith('_df.csv')]
 
 
 facility_name = st.selectbox("Choose a Facility", options, index=0, placeholder="Choose an option")
@@ -101,16 +81,16 @@ facility_name = st.selectbox("Choose a Facility", options, index=0, placeholder=
 if facility_name:
     # Normalize facility name to match file naming convention
     sanitized_facility_name = re.sub(r'\W+', '_', facility_name).upper()
-    complete_file_path = f'https://raw.githubusercontent.com/lksanterre/prison/main/clean_data/{sanitized_facility_name}_df.csv'
-
+    #complete_file_path = os.path.join('/Users/lancesanterre/prison_proj/prison/clean_data/', sanitized_facility_name + "_df.csv")
+    complete_file_path = f'https://raw.githubusercontent.com/lksanterre/prison/main/clean_data/{sanitized_view_name}_df.csv'
     try:
         # Loading the data into a DataFrame
         data_df = pd.read_csv(complete_file_path)
 
         # Convert the 'datetime_of_data' column to datetime type for proper sorting
         if 'datetime_of_data' in data_df.columns and 'visiting_status' in data_df.columns:
-            data_df['datetime_of_data'] = data_df['datetime_of_data'].str[:-4]
             data_df['datetime_of_data'] = pd.to_datetime(data_df['datetime_of_data'])
+            data_df.sort_values(by='datetime_of_data', inplace=True)
 
             # Plot with Plotly - creating a line chart for population
             fig = go.Figure()
@@ -143,3 +123,53 @@ if facility_name:
         st.error(f"Data file for {facility_name} not found.")
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
+
+def sanitize_name(name):
+    """Sanitize names for consistent comparison."""
+    # Convert to lower case, strip whitespace, and replace special characters if needed
+    sanitized = name.lower().strip().replace(' ', '_')  # Adjust this based on your specific needs
+    return sanitized
+new_df =  pd.read_csv('https://raw.githubusercontent.com/lksanterre/prison/main/facilities/facilities.csv')
+
+fig = go.Figure()
+selected_facility = sanitize_name(facility_name)
+for index, row in new_df.iterrows():
+    # Sanitize the name from the DataFrame for comparison
+    sanitized_name = sanitize_name(row['name'])
+    color = 'red' if sanitized_name == selected_facility else 'blue'
+    fig.add_trace(go.Scattermapbox(
+        lon=[row['long']],
+        lat=[row['lat']],
+        mode='markers+text',  # For adding text labels beside markers
+        marker=go.scattermapbox.Marker(
+            size=9,
+            color=color,
+            opacity=1 if color == 'blue' else 0.9
+        ),
+        text=row['name'],  # Original name for display
+        hoverinfo='text+name',
+        #hovertext="<b style='color:white;'>" + row['name'] + "</b>",  # Attempt to make hover text white
+        name=row['name']
+    ))
+mapbox_access_token = 'pk.eyJ1IjoibGtzYW50ZXJyZSIsImEiOiJjbHMwdWZwaGswNHZwMmtucTZvdXQ0dmQ4In0.atoTB2hBdxOrfAszlnLEfg'
+
+# Set mapbox and layout attributes
+fig.update_layout(
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        zoom=3,
+        center=dict(lat=37.0902, lon=-95.7129),
+        style='streets'
+    ),
+    showlegend=False,
+    title='Interactive Map of Locations',
+    hoverlabel=dict(
+        bgcolor="black",  # Background color for hover
+        font_color="white",  # Font color for hover text
+        font_size=12,
+        font_family="Arial"
+    )
+)
+
+# Streamlit: Display the Plotly figure
+st.plotly_chart(fig, use_container_width=True)
